50690964478de83e16fbf490c415ea87e3faa997
Ivan Utkin <iutkin@ethz.ch>
2023-11-01 15:25:35 +0100

    Convert to OffsetArrays

--- braeck_2D_PT_acc_ka.jl
+++ braeck_2D_PT_acc_ka.jl
@@ -4,113 +4,140 @@ using CUDA
 CUDA.allowscalar(false)
 CUDA.device!(5)
 
-@views av1(a) = 0.5 .* (a[1:end-1] .+ a[2:end])
-@views avx(a) = 0.5 .* (a[1:end-1, :] .+ a[2:end, :])
-@views avy(a) = 0.5 .* (a[:, 1:end-1] .+ a[:, 2:end])
-@views av4(a) = 0.25 .* (a[1:end-1, 1:end-1] .+ a[2:end, 1:end-1] .+ a[2:end, 2:end] .+ a[1:end-1, 2:end])
+@views av1(a) = 0.5 .* (a[begin:end-1] .+ a[begin+1:end])
+@views avx(a) = 0.5 .* (a[begin:end-1, :] .+ a[begin+1:end, :])
+@views avy(a) = 0.5 .* (a[:, begin:end-1] .+ a[:, begin+1:end])
+@views av4(a) = 0.25 .* (a[begin+0:end-1, begin+0:end-1] .+
+                         a[begin+1:end+0, begin+0:end-1] .+
+                         a[begin+1:end+0, begin+1:end+0] .+
+                         a[begin+0:end-1, begin+1:end+0])
 
-@kernel function update_σ!(Pr, τxx, τyy, τxy, τxx_old, τyy_old, τxy_old, Vx, Vy, μ, G, dt, dτ_r, r, θ_dτ, dx, dy)
+@kernel function update_σ!(Pr, τ, τ_old, V, μ, G, dt, dτ_r, r, θ_dτ, dx, dy, nx, ny)
     ix, iy = @index(Global, NTuple)
-    @inbounds if checkbounds(Bool, Pr, ix, iy)
-        ε̇xx = (Vx[ix+1, iy+1] - Vx[ix, iy+1]) / dx
-        ε̇yy = (Vy[ix+1, iy+1] - Vy[ix+1, iy]) / dy
+    @inbounds if ix <= nx && iy <= ny
+        ε̇xx = (V.x[ix+1, iy] - V.x[ix, iy]) / dx
+        ε̇yy = (V.y[ix, iy+1] - V.y[ix, iy]) / dy
         ∇V   = ε̇xx + ε̇yy
-        μ_av = 0.25 * (μ[ix, iy] + μ[ix+1, iy] + μ[ix+1, iy+1] + μ[ix, iy+1])
-        μ_ve = 1 / (1 / (G * dt) + 1 / μ_av)
+        μ_c  = 0.25 * (μ[ix, iy] + μ[ix+1, iy] + μ[ix+1, iy+1] + μ[ix, iy+1])
+        μ_ve = 1 / (1 / (G * dt) + 1 / μ_c)
         # update pressure
         Pr[ix, iy] -= ∇V * μ_ve * (r / θ_dτ)
         # diagonal deviatoric stress
-        dτxx_dt = (τxx[ix, iy] - τxx_old[ix, iy]) / dt
-        dτyy_dt = (τyy[ix, iy] - τyy_old[ix, iy]) / dt
-        τxx[ix, iy] += (-dτxx_dt / G - τxx[ix, iy] / μ_av + 2.0 * (ε̇xx - ∇V / 3.0)) * μ_ve * dτ_r
-        τyy[ix, iy] += (-dτyy_dt / G - τyy[ix, iy] / μ_av + 2.0 * (ε̇yy - ∇V / 3.0)) * μ_ve * dτ_r
+        dτxx_dt = (τ.xx[ix, iy] - τ_old.xx[ix, iy]) / dt
+        dτyy_dt = (τ.yy[ix, iy] - τ_old.yy[ix, iy]) / dt
+        τ.xx[ix, iy] += (-dτxx_dt / G - τ.xx[ix, iy] / μ_c + 2.0 * (ε̇xx - ∇V / 3.0)) * μ_ve * dτ_r
+        τ.yy[ix, iy] += (-dτyy_dt / G - τ.yy[ix, iy] / μ_c + 2.0 * (ε̇yy - ∇V / 3.0)) * μ_ve * dτ_r
     end
-    @inbounds if checkbounds(Bool, τxy, ix, iy)
-        ε̇xy = 0.5 * ((Vx[ix, iy+1] - Vx[ix, iy]) / dy + (Vy[ix+1, iy] - Vy[ix, iy]) / dx)
+    @inbounds if ix <= nx + 1 && iy <= ny + 1
+        ε̇xy = 0.5 * ((V.x[ix, iy] - V.x[ix, iy-1]) / dy + (V.y[ix, iy] - V.y[ix-1, iy]) / dx)
         μ_ve = 1 / (1 / (G * dt) + 1 / μ[ix, iy])
-        dτxy_dt = (τxy[ix, iy] - τxy_old[ix, iy]) / dt
-        τxy[ix, iy] += (-dτxy_dt / G - τxy[ix, iy] / μ[ix, iy] + 2.0 * ε̇xy) * μ_ve * dτ_r
+        dτxy_dt = (τ.xy[ix, iy] - τ_old.xy[ix, iy]) / dt
+        τ.xy[ix, iy] += (-dτxy_dt / G - τ.xy[ix, iy] / μ[ix, iy] + 2.0 * ε̇xy) * μ_ve * dτ_r
     end
 end
 
-@kernel function compute_τII!(τII, τxx, τyy, τxy)
+@kernel function compute_τII!(τII, τ)
     ix, iy = @index(Global, NTuple)
     @inbounds begin
-        τxx_av = 0.25 * (τxx[ix, iy] + τxx[ix+1, iy] + τxx[ix+1, iy+1] + τxx[ix, iy+1])
-        τyy_av = 0.25 * (τyy[ix, iy] + τyy[ix+1, iy] + τyy[ix+1, iy+1] + τyy[ix, iy+1])
-        τII[ix+1, iy+1] = sqrt(0.5 * (τxx_av^2 + τyy_av^2) + τxy[ix+1, iy+1]^2)
+        τxx_av = 0.25 * (τ.xx[ix-1, iy-1] + τ.xx[ix, iy-1] + τ.xx[ix, iy] + τ.xx[ix-1, iy])
+        τyy_av = 0.25 * (τ.yy[ix-1, iy-1] + τ.yy[ix, iy-1] + τ.yy[ix, iy] + τ.yy[ix-1, iy])
+        τII[ix, iy] = sqrt(0.5 * (τxx_av^2 + τyy_av^2) + τ.xy[ix, iy]^2)
     end
 end
 
 @kernel function compute_μ!(μ, τII, T, A, E_R, npow)
     ix, iy = @index(Global, NTuple)
     @inbounds begin
-        T_av = 0.25 * (T[ix, iy] + T[ix+1, iy] + T[ix+1, iy+1] + T[ix, iy+1])
+        T_av = 0.25 * (T[ix-1, iy-1] + T[ix, iy-1] + T[ix, iy] + T[ix-1, iy])
         μ[ix, iy] = (1 / A) * exp(E_R / T_av) * τII[ix, iy]^(1 - npow)
     end
 end
 
-@kernel function update_V!(Vx, Vy, Pr, τxx, τyy, τxy, μ, G, dt, nudτ, dx, dy)
+@kernel function update_V!(V, Pr, τ, μ, G, dt, nudτ, dx, dy, nx, ny)
     ix, iy = @index(Global, NTuple)
-    @inbounds if ix <= size(Vx, 1) - 2 && iy <= size(Vx, 2) - 2
-        μ_mloc = max(μ[ix+0, iy+0], μ[ix+1, iy+0], μ[ix+2, iy+0],
-                     μ[ix+0, iy+1], μ[ix+1, iy+1], μ[ix+2, iy+1])
-        ∂σxx_∂x = -(Pr[ix+1, iy] - Pr[ix, iy]) / dx + (τxx[ix+1, iy] - τxx[ix, iy]) / dx
-        ∂τxy_∂y = (τxy[ix+1, iy+1] - τxy[ix+1, iy]) / dy
-        Vx[ix+1, iy+1] += (∂σxx_∂x + ∂τxy_∂y) * (1 / (G * dt) + 1 / μ_mloc) * nudτ
+    @inbounds if ix <= nx + 1 && iy <= ny
+        μ_mloc = max(μ[ix-1, iy+0], μ[ix, iy+0], μ[ix+1, iy+0],
+                     μ[ix-1, iy+1], μ[ix, iy+1], μ[ix+1, iy+1])
+        ∂σxx_∂x = -(Pr[ix, iy] - Pr[ix-1, iy]) / dx + (τ.xx[ix, iy] - τ.xx[ix-1, iy]) / dx
+        ∂τxy_∂y = (τ.xy[ix, iy+1] - τ.xy[ix, iy]) / dy
+        V.x[ix, iy] += (∂σxx_∂x + ∂τxy_∂y) * (1 / (G * dt) + 1 / μ_mloc) * nudτ
     end
-    @inbounds if ix <= size(Vy, 1) - 2 && iy <= size(Vy, 2) - 2
-        μ_mloc = max(μ[ix+0, iy+0], μ[ix+0, iy+1], μ[ix+0, iy+2],
-                     μ[ix+1, iy+0], μ[ix+1, iy+1], μ[ix+1, iy+2])
-        ∂σyy_∂y = -(Pr[ix, iy+1] - Pr[ix, iy]) / dy + (τyy[ix, iy+1] - τyy[ix, iy]) / dy
-        ∂τxy_∂x = (τxy[ix+1, iy+1] - τxy[ix, iy+1]) / dx
-        Vy[ix+1, iy+1] += (∂σyy_∂y + ∂τxy_∂x) * (1 / (G * dt) + 1 / μ_mloc) * nudτ
+    @inbounds if ix <= nx && iy <= ny + 1
+        μ_mloc = max(μ[ix+0, iy-1], μ[ix+0, iy], μ[ix+0, iy+1],
+                     μ[ix+1, iy-1], μ[ix+1, iy], μ[ix+1, iy+1])
+        ∂σyy_∂y = -(Pr[ix, iy] - Pr[ix, iy-1]) / dy + (τ.yy[ix, iy] - τ.yy[ix, iy-1]) / dy
+        ∂τxy_∂x = (τ.xy[ix+1, iy] - τ.xy[ix, iy]) / dx
+        V.y[ix, iy] += (∂σyy_∂y + ∂τxy_∂x) * (1 / (G * dt) + 1 / μ_mloc) * nudτ
     end
 end
 
-@kernel function compute_qT!(qTx, qTy, T, χ, dx, dy)
+@kernel function compute_residuals!(res_Pr, res_V, Pr, τ, V, dx, dy, nx, ny)
     ix, iy = @index(Global, NTuple)
-    @inbounds if ix <= size(qTx, 1) - 2 && iy <= size(qTx, 2)
-        qTx[ix+1, iy] = -χ * (T[ix+2, iy+1] - T[ix+1, iy+1]) / dx
+    @inbounds if ix <= nx && iy <= ny
+        ε̇xx = (V.x[ix+1, iy] - V.x[ix, iy]) / dx
+        ε̇yy = (V.y[ix, iy+1] - V.y[ix, iy]) / dy
+        res_Pr[ix, iy] = ε̇xx + ε̇yy
     end
-    @inbounds if ix <= size(qTy, 1) && iy <= size(qTy, 2) - 2
-        qTy[ix, iy+1] = -χ * (T[ix+1, iy+2] - T[ix+1, iy+1]) / dy
+    @inbounds if ix <= nx + 1 && iy <= ny
+        ∂σxx_∂x = -(Pr[ix, iy] - Pr[ix-1, iy]) / dx + (τ.xx[ix, iy] - τ.xx[ix-1, iy]) / dx
+        ∂τxy_∂y = (τ.xy[ix, iy+1] - τ.xy[ix, iy]) / dy
+        res_V.x[ix, iy] = ∂σxx_∂x + ∂τxy_∂y
+    end
+    @inbounds if ix <= nx && iy <= ny + 1
+        ∂σyy_∂y = -(Pr[ix, iy] - Pr[ix, iy-1]) / dy + (τ.yy[ix, iy] - τ.yy[ix, iy-1]) / dy
+        ∂τxy_∂x = (τ.xy[ix+1, iy] - τ.xy[ix, iy]) / dx
+        res_V.y[ix, iy] = ∂σyy_∂y + ∂τxy_∂x
     end
 end
 
-@kernel function update_T!(T, T_old, qTx, qTy, τII, μ, C, dt, dx, dy)
+@kernel function init_T!(T, Tbg, T0, xc, yc, h)
+    ix, iy = @index(Global, NTuple)
+    @inbounds T[ix, iy] = abs(xc[ix]) <= h / 2 ? T0 : Tbg
+end
+
+@kernel function compute_qT!(qT, T, χ, dx, dy, nx, ny)
+    ix, iy = @index(Global, NTuple)
+    @inbounds if ix <= nx + 1 && iy <= ny
+        qT.x[ix, iy] = -χ * (T[ix, iy] - T[ix-1, iy]) / dx
+    end
+    @inbounds if ix <= nx && iy <= ny + 1
+        qT.y[ix, iy] = -χ * (T[ix, iy] - T[ix, iy-1]) / dy
+    end
+end
+
+@kernel function update_T!(T, T_old, qT, τII, μ, C, dt, dx, dy)
     ix, iy = @index(Global, NTuple)
     @inbounds begin
         sh = 0.25 * (τII[ix+0, iy+0]^2 / μ[ix+0, iy+0] +
                      τII[ix+1, iy+0]^2 / μ[ix+1, iy+0] +
                      τII[ix+1, iy+1]^2 / μ[ix+1, iy+1] +
                      τII[ix+0, iy+1]^2 / μ[ix+0, iy+1])
-        divqT = (qTx[ix+1, iy] - qTx[ix, iy]) / dx + (qTy[ix, iy+1] - qTy[ix, iy]) / dy
-        T[ix+1, iy+1] = T_old[ix+1, iy+1] + dt * (-divqT + (1 / C) * sh)
+        divqT = (qT.x[ix+1, iy] - qT.x[ix, iy]) / dx + (qT.y[ix, iy+1] - qT.y[ix, iy]) / dy
+        T[ix, iy] = T_old[ix, iy] + dt * (-divqT + (1 / C) * sh)
     end
 end
 
 @kernel function neumann_bc_x!(A)
     iy = @index(Global, Linear)
-    @inbounds A[1, iy] = A[2, iy]
+    @inbounds A[begin, iy] = A[begin+1, iy]
     @inbounds A[end, iy] = A[end-1, iy]
 end
 
 @kernel function neumann_bc_y!(A)
     ix = @index(Global, Linear)
-    @inbounds A[ix, 1] = A[ix, 2]
+    @inbounds A[ix, begin] = A[ix, begin+1]
     @inbounds A[ix, end] = A[ix, end-1]
 end
 
 @kernel function dirichlet_bc_x!(A)
     iy = @index(Global, Linear)
-    @inbounds A[1, iy] = -A[2, iy]
+    @inbounds A[begin, iy] = -A[begin+1, iy]
     @inbounds A[end, iy] = -A[end-1, iy]
 end
 
 @kernel function dirichlet_bc_y!(A)
     ix = @index(Global, Linear)
-    @inbounds A[ix, 1] = -A[ix, 2]
+    @inbounds A[ix, begin] = -A[ix, begin+1]
     @inbounds A[ix, end] = -A[ix, end-1]
 end
 
@@ -188,36 +215,30 @@ end
     dτ_r       = 1.0 / (θ_dτ + 1.0)
     # fields
     # thermo
-    T     = KernelAbstractions.zeros(backend, Float64, nx + 2, ny + 2)
-    T_old = KernelAbstractions.zeros(backend, Float64, nx + 2, ny + 2)
-    qTx   = KernelAbstractions.zeros(backend, Float64, nx + 1, ny)
-    qTy   = KernelAbstractions.zeros(backend, Float64, nx, ny + 1)
+    T     = scalar_field(backend, Float64, nx, ny)
+    T_old = scalar_field(backend, Float64, nx, ny)
+    qT    = vector_field(backend, Float64, nx, ny)
     # mechanics
-    Pr      = KernelAbstractions.zeros(backend, Float64, nx, ny)
-    τxx     = KernelAbstractions.zeros(backend, Float64, nx, ny)
-    τyy     = KernelAbstractions.zeros(backend, Float64, nx, ny)
-    τxy     = KernelAbstractions.zeros(backend, Float64, nx + 1, ny + 1)
-    τII     = KernelAbstractions.zeros(backend, Float64, nx + 1, ny + 1)
-    τxx_old = KernelAbstractions.zeros(backend, Float64, nx, ny)
-    τyy_old = KernelAbstractions.zeros(backend, Float64, nx, ny)
-    τxy_old = KernelAbstractions.zeros(backend, Float64, nx + 1, ny + 1)
-    Vx      = KernelAbstractions.zeros(backend, Float64, nx + 1, ny + 2)
-    Vy      = KernelAbstractions.zeros(backend, Float64, nx + 2, ny + 1)
-    ∇V      = KernelAbstractions.zeros(backend, Float64, nx, ny)
-    μ       = KernelAbstractions.zeros(backend, Float64, nx + 1, ny + 1)
+    Pr    = scalar_field(backend, Float64, nx, ny)
+    τ     = tensor_field(backend, Float64, nx, ny)
+    τ_old = tensor_field(backend, Float64, nx, ny)
+    τII   = scalar_field(backend, Float64, nx + 1, ny + 1)
+    V     = vector_field(backend, Float64, nx, ny)
+    μ     = scalar_field(backend, Float64, nx + 1, ny + 1)
+    # residuals
+    res_Pr = scalar_field(backend, Float64, nx, ny)
+    res_V  = vector_field(backend, Float64, nx, ny)
     # initialisation
-    broadcast!(T[2:end-1, 2:end-1], xc, yc') do x, y
-        abs(x) <= h / 2 ? T0 : Tbg
-    end
-    T[[1, end], :] .= T[[2, end - 1], :]
-    T[:, [1, end]] .= T[:, [2, end - 1]]
-    τxy .= σ0
-    τII[2:end-1, 2:end-1] .= sqrt.(0.5 .* (av4(τxx) .^ 2 .+ av4(τyy) .^ 2) .+ τxy[2:end-1, 2:end-1] .^ 2)
-    τII[[1, end], :] .= τII[[2, end - 1], :]
-    τII[:, [1, end]] .= τII[:, [2, end - 1]]
+    init_T!(backend, 256, (nx, ny))(T, Tbg, T0, xc, yc, h)
+    neumann_bc_x!(backend, 256, ny + 2)(T)
+    neumann_bc_y!(backend, 256, nx + 2)(T)
+    τ.xy .= σ0
+    compute_τII!(backend, 256, (nx + 1, ny + 1))(τII, τ)
+    neumann_bc_x!(backend, 256, ny + 3)(τII)
+    neumann_bc_y!(backend, 256, nx + 3)(τII)
     # temporal evolution
     time_evo = Float64[0.0]
-    Tmax_evo = Float64[maximum(T)/E_R]
+    Tmax_evo = Float64[maximum(parent(T))/E_R]
     σ_evo    = Float64[1.0]
     Vmax_evo = Float64[0.0]
     sizehint!(time_evo, nt + 1)
@@ -234,12 +255,12 @@ end
            σ_evo    = Axis(gl[2, 1]; xlabel=L"t/\tau_r", ylabel=L"\sigma/\sigma_0"),
            Tmax_evo = Axis(gl[3, 1]; xlabel=L"t/\tau_r", ylabel=L"T_\mathrm{max}/(E/R)", yscale=log10),
            Vmax_evo = Axis(gl[4, 1]; xlabel=L"t/\tau_r", ylabel=L"V_\mathrm{max}/(\sigma_0 h/\mu_0)", yscale=log10))
-    plts = (T        = heatmap!(axs.T, xc, yc, Array(T[2:end-1, 2:end-1] ./ E_R); colormap=:turbo),
-            T_ini_sl = lines!(axs.T_sl, Point2.(xc ./ h, Array(T[2:end-1, ny÷2] ./ E_R)); linewidth=4),
-            T_sl     = lines!(axs.T_sl, Point2.(xc ./ h, Array(T[2:end-1, ny÷2] ./ E_R)); linewidth=4),
+    plts = (T        = heatmap!(axs.T, xc, yc, Array(parent(T)[2:end-1, 2:end-1] ./ E_R); colormap=:turbo),
+            T_ini_sl = lines!(axs.T_sl, Point2.(xc ./ h, Array(parent(T)[2:end-1, ny÷2] ./ E_R)); linewidth=4),
+            T_sl     = lines!(axs.T_sl, Point2.(xc ./ h, Array(parent(T)[2:end-1, ny÷2] ./ E_R)); linewidth=4),
             Tmaxa    = hlines!(axs.T_sl, Tmaxa; linewidth=4, color=:gray, linestyle=:dash),
-            Vy_sl    = lines!(axs.Vy_sl, Point2.(xc ./ h, Array(Vy[2:end-1, ny÷2])); linewidth=4),
-            σ_evo    = lines!(axs.σ_evo, Point2.(time_evo, maximum(τxy) / σ0); linewidth=4),
+            Vy_sl    = lines!(axs.Vy_sl, Point2.(xc ./ h, Array(parent(V.y)[2:end-1, ny÷2])); linewidth=4),
+            σ_evo    = lines!(axs.σ_evo, Point2.(time_evo, maximum(parent(τ.xy)) / σ0); linewidth=4),
             Tmax_evo = lines!(axs.Tmax_evo, Point2.(time_evo, Tmax_evo ./ E_R); linewidth=4),
             Vmax_evo = lines!(axs.Vmax_evo, Point2.(time_evo, Vmax_evo); linewidth=4))
     limits!(axs.T_sl, -2, 2, 0.5Tbg_E_R, 2Tmaxa)
@@ -254,35 +275,39 @@ end
     # iframe = 0
     while tcur < ttot
         println("it = $it")
-        τxx_old .= τxx
-        τyy_old .= τyy
-        τxy_old .= τxy
-        T_old .= T
+        copyto!(parent(τ_old.xx), parent(τ.xx))
+        copyto!(parent(τ_old.yy), parent(τ.yy))
+        copyto!(parent(τ_old.xy), parent(τ.xy))
+        copyto!(parent(T_old   ), parent(T))
         # time step
-        dσ_dt = maximum(abs.(-(A * G) .* τII .^ npow .* exp.(-E_R ./ av4(T)) .+ G .* (diff(Vx; dims=2) ./ dy .+ diff(Vy; dims=1) ./ dx)))
+        dσ_dt = maximum(abs.(-(A * G) .* parent(τII)[2:end-1,2:end-1] .^ npow .* exp.(-E_R ./ av4(parent(T))) .+ G .* (diff(parent(V.x)[2:end-1,:]; dims=2) ./ dy .+ diff(parent(V.y)[:,2:end-1]; dims=1) ./ dx)))
         dt = min(1e-4 * σ0 / dσ_dt, dt_diff, 1e-3 * τr)
         # iteration loop
         for iter in 1:niter
             # stress
-            compute_τII!(backend, 256, (nx - 1, ny - 1))(τII, τxx, τyy, τxy)
-            neumann_bc_x!(backend, 256, ny + 1)(τII)
-            neumann_bc_y!(backend, 256, nx + 1)(τII)
+            compute_τII!(backend, 256, (nx + 1, ny + 1))(τII, τ)
+            neumann_bc_x!(backend, 256, ny + 3)(τII)
+            neumann_bc_y!(backend, 256, nx + 3)(τII)
             compute_μ!(backend, 256, (nx + 1, ny + 1))(μ, τII, T, A, E_R, npow)
-            update_σ!(backend, 256, (nx + 1, ny + 1))(Pr, τxx, τyy, τxy, τxx_old, τyy_old, τxy_old, Vx, Vy, μ, G, dt, dτ_r, r, θ_dτ, dx, dy)
+            update_σ!(backend, 256, (nx + 1, ny + 1))(Pr, τ, τ_old, V, μ, G, dt, dτ_r, r, θ_dτ, dx, dy, nx, ny)
             # velocity
-            update_V!(backend, 256, (nx, ny))(Vx, Vy, Pr, τxx, τyy, τxy, μ, G, dt, nudτ, dx, dy)
-            dirichlet_bc_y!(backend, 256, nx + 2)(Vx)
-            dirichlet_bc_x!(backend, 256, ny + 2)(Vy)
+            update_V!(backend, 256, (nx, ny))(V, Pr, τ, μ, G, dt, nudτ, dx, dy, nx, ny)
+            parent(V.x)[[2, nx + 2], :] .= 0.0
+            parent(V.y)[:, [2, ny + 2]] .= 0.0
+            dirichlet_bc_x!(backend, 256, ny + 2)(V.y)
+            dirichlet_bc_y!(backend, 256, nx + 2)(V.x)
             # temperature
-            compute_qT!(backend, 256, (nx, ny))(qTx, qTy, T, χ, dx, dy)
-            update_T!(backend, 256, (nx, ny))(T, T_old, qTx, qTy, τII, μ, C, dt, dx, dy)
+            compute_qT!(backend, 256, (nx + 1, ny + 1))(qT, T, χ, dx, dy, nx, ny)
+            update_T!(backend, 256, (nx, ny))(T, T_old, qT, τII, μ, C, dt, dx, dy)
             neumann_bc_x!(backend, 256, ny + 2)(T)
             neumann_bc_y!(backend, 256, nx + 2)(T)
             if iter % ncheck == 0
-                ∇V .= diff(Vx[:, 2:end-1]; dims=1) ./ dx .+ diff(Vy[2:end-1, :]; dims=2) ./ dy
-                err_Pr = maximum(abs.(∇V)) / (σ0^npow * A * exp(-E_R / T0))
-                err_Vx = maximum(abs.(.-diff(Pr; dims=1) ./ dx .+ diff(τxx; dims=1) ./ dx .+ diff(τxy[2:end-1, :]; dims=2) ./ dy)) / σ0 * h
-                err_Vy = maximum(abs.(.-diff(Pr; dims=2) ./ dy .+ diff(τyy; dims=2) ./ dy .+ diff(τxy[:, 2:end-1]; dims=1) ./ dx)) / σ0 * h
+                compute_residuals!(backend, 256, (nx + 1, ny + 1))(res_Pr, res_V, Pr, τ, V, dx, dy, nx, ny)
+                parent(res_V.x)[[2, nx + 2], :] .= 0.0
+                parent(res_V.y)[:, [2, ny + 2]] .= 0.0
+                err_Pr = maximum(abs.(parent(res_Pr))) / (σ0^npow * A * exp(-E_R / T0))
+                err_Vx = maximum(abs.(parent(res_V.x))) / σ0 * h
+                err_Vy = maximum(abs.(parent(res_V.y))) / σ0 * h
                 @printf("  iter / nx = %.1f, err: [Pr = %1.3e, Vx = %1.3e; Vy = %1.3e]\n", iter / nx, err_Pr, err_Vx, err_Vy)
                 if !isfinite(err_Pr) || !isfinite(err_Vx) || !isfinite(err_Vy)
                     error("simulation failed")
@@ -298,19 +323,19 @@ end
         it   += 1
         # evolution
         push!(time_evo, tcur / τr)
-        push!(Tmax_evo, maximum(T) / E_R)
-        push!(Vmax_evo, maximum(Vy) / (σ0^npow * h * A * exp(-E_R / T0)))
-        push!(σ_evo, maximum(τII) / σ0)
+        push!(Tmax_evo, maximum(parent(T)) / E_R)
+        push!(Vmax_evo, maximum(parent(V.y)) / (σ0^npow * h * A * exp(-E_R / T0)))
+        push!(σ_evo, maximum(parent(τII)) / σ0)
         # convergence check
-        if any(.!isfinite.(T)) || any(.!isfinite.(τII))
+        if any(.!isfinite.(parent(T))) || any(.!isfinite.(parent(τII)))
             error("simulation failed")
         end
         # visualisation
         if it % nvis == 0
             # plots
-            plts.T[3]        = Array(T[2:end-1, 2:end-1] ./ E_R)
-            plts.T_sl[1]     = Point2.(xc ./ h, Array(T[2:end-1, ny÷2] ./ E_R))
-            plts.Vy_sl[1]    = Point2.(xc ./ h, Array(Vy[2:end-1, ny÷2]))
+            plts.T[3]        = Array(parent(T)[2:end-1, 2:end-1] ./ E_R)
+            plts.T_sl[1]     = Point2.(xc ./ h, Array(parent(T)[2:end-1, ny÷2] ./ E_R))
+            plts.Vy_sl[1]    = Point2.(xc ./ h, Array(parent(V.y)[2:end-1, ny÷2]))
             plts.σ_evo[1]    = Point2.(time_evo, σ_evo)
             plts.Tmax_evo[1] = Point2.(time_evo, Tmax_evo)
             plts.Vmax_evo[1] = Point2.(time_evo, Vmax_evo)
